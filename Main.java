0cc8dcadee27239decf1ddcfe93c463b91730d68
Rumburaq2 <oliver.hurt@student.gyarab.cz>
2024-10-24 18:08:26 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,48 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Integer i = 0;
+        Zasobnik<Integer> zasobnink = new Zasobnik();
+
+        System.out.println(zasobnink);
+        zasobnink.push(6);
+        System.out.println(zasobnink);
+        zasobnink.push(7);
+        System.out.println(zasobnink);
+        i = zasobnink.pop();
+        System.out.println(i);
+        zasobnink.push(8);
+        System.out.println(zasobnink);
+
+        Zasobnik<String > zasobnink2 = new Zasobnik();
+
+        System.out.println(zasobnink2);
+        zasobnink2.push("hello");
+        System.out.println(zasobnink2);
+        zasobnink2.push("world");
+        System.out.println(zasobnink2);
+        String s = zasobnink2.pop();
+        System.out.println(s);
+        zasobnink2.push("dsfdfds");
+        System.out.println(zasobnink2);
+
+        System.out.println("-------------------");
+
+
+        ZasobnikPole<Integer> zasobninkPole = new ZasobnikPole<>(5);
+
+        System.out.println(zasobninkPole);
+        zasobninkPole.push(6);
+        System.out.println(zasobninkPole);
+        zasobninkPole.push(7);
+        System.out.println(zasobninkPole);
+        int x = zasobninkPole.pop();
+        System.out.println(x);
+        System.out.println(zasobninkPole);
+        zasobninkPole.push(8);
+        System.out.println(zasobninkPole);
+
+
+    }
+}
---
+++ ZasobnikPole.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class ZasobnikPole<T> {
+
+    private T[] arr;
+    private int index;
+
+    public ZasobnikPole(int size) {
+        this.arr = (T[]) new Object[size];
+        this.index = -1; //prvni polozke inkrementuje a da prvni prvek na index 0
+    }
+
+    public void push(T elem) {
+        this.index++;
+        arr[this.index] = elem;
+
+    }
+
+    public T pop() {
+        T elem = arr[index];
+        arr[index] = null;
+        this.index--;
+        return elem;
+    }
+
+    @Override
+    public String toString() {
+        return "ZasobnikPole{" +
+                "arr=" + Arrays.toString(arr) +
+                ", index=" + index +
+                '}';
+    }
+}
